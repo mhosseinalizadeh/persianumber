12834ad14ff4c669efc5cda25ce54adfa47c62fc
aghaomid <omid.mymail@gmail.com>
2013-10-23 16:08:54 +0330

    Adding convert to arabic number.

--- persianumber.js
+++ persianumber.js
@@ -1,40 +1,29 @@
+var defaultSettings = 'fa';
 (function( $ ){
-  $.fn.persiaNumber = function() {
-	  var groupSelection = this;
-	  for(i=0; i< groupSelection.length ; i++){
-		  var htmlTxt = $(groupSelection[i]).html();
-		  var trueTxt = convertDecimalPoint(htmlTxt);
-		  trueTxt = convertToPersianNum(trueTxt);
-		  $(groupSelection[i]).html(trueTxt);
+  $.fn.persiaNumber = function(settings) {
+	  if(typeof(settings) == 'string' && settings.length > 0)
+		  defaultSettings = settings;
+	  var range = 1728;
+	  if(settings == 'ar'){
+		  range = 1584;
 	  }
-function convertToPersianNum(htmlTxt){
-		  var otIndex = htmlTxt.indexOf("<");
-		  var ctIndex = htmlTxt.indexOf(">");
-		  if(otIndex == -1 && ctIndex == -1 && htmlTxt.length > 0){
-			var trueTxt = htmlTxt.replace(/1/gi, "۱").replace(/2/gi, "۲").replace(/3/gi, "۳").replace(/4/gi, "۴").replace(/5/gi, "۵").replace(/6/gi, "۶").replace(/7/gi, "۷").replace(/8/gi, "۸").replace(/9/gi, "۹").replace(/0/gi, "۰");
-			return trueTxt;
-		  }
-			  var tag = htmlTxt.substring(otIndex,ctIndex + 1);
-			  var str = htmlTxt.substring(0,otIndex);
-			  str = convertDecimalPoint(str);
-			  str = str.replace(/1/gi, "۱").replace(/2/gi, "۲").replace(/3/gi, "۳").replace(/4/gi, "۴").replace(/5/gi, "۵").replace(/6/gi, "۶").replace(/7/gi, "۷").replace(/8/gi, "۸").replace(/9/gi, "۹").replace(/0/gi, "۰");
-			  var refinedHtmlTxt = str + tag;
-			  var htmlTxt = htmlTxt.substring(ctIndex + 1, htmlTxt.length);
-			  if(htmlTxt.length > 0 && otIndex != -1 || ctIndex != -1){
-				  var trueTxt = refinedHtmlTxt;
-				  var trueTxt =  trueTxt + convertToPersianNum(htmlTxt); 
-			  }else{
-					return refinedHtmlTxt+ htmlTxt;
-			  }
-			  return trueTxt; 		  
-
-	}
-function convertDecimalPoint(str){
-	for(j=1;j<str.length - 1; j++){
-		if(str.charCodeAt(j-1) > 47 &&  str.charCodeAt(j-1) < 58 && str.charCodeAt(j+1) > 47 &&  str.charCodeAt(j+1) < 58 && str.charCodeAt(j) == 46)
-			str = str.substring(0,j) + '٫' + str.substring(j+1,str.length);
-	}
-	return str;
-}
+	  window.persiaNumberedDOM = this;
+	  convert(this, range);
+	  $(document).ajaxComplete(function(){
+		  var thisObj = window.persiaNumberedDOM;
+		  convert(thisObj, range);
+	  });
+	function convert(obj, range){
+		  obj.find("*").andSelf().contents().each(function() {
+			    if (this.nodeType === 3) {
+				    this.nodeValue = this.nodeValue.replace(this.nodeValue.match(/[0-9]*\.[0-9]+/), function(txt){
+					    return txt.replace(/\./,',');
+					});		    	
+			        this.nodeValue = this.nodeValue.replace(/\d/g, function(v) {
+			            return String.fromCharCode(v.charCodeAt(0) + range);
+			        });
+			    }
+			});		
+	}  
 };
 })( jQuery );
