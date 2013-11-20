5ef27be83156efd1d7e19d6e5492cdf4beae1787
aghaomid <omid.mymail@gmail.com>
2013-11-20 15:12:09 +0330

    Fixing style tag problem and refine parseInt & pareFloat function for Persian and Arabic numbers.

--- persianumber.js
+++ persianumber.js
@@ -1,5 +1,6 @@
 var defaultSettings = 'fa';
 (function( $ ){
+	
   $.fn.persiaNumber = function(settings) {
 	  if(typeof(settings) == 'string' && settings.length > 0)
 		  defaultSettings = settings;
@@ -15,7 +16,7 @@ var defaultSettings = 'fa';
 	  });
 	function convert(obj, range){
 		  obj.find("*").andSelf().contents().each(function() {
-			    if (this.nodeType === 3) {
+			    if (this.nodeType === 3 && this.parentNode.localName != "style") {
 				    this.nodeValue = this.nodeValue.replace(this.nodeValue.match(/[0-9]*\.[0-9]+/), function(txt){
 					    return txt.replace(/\./,',');
 					});		    	
@@ -27,3 +28,13 @@ var defaultSettings = 'fa';
 	}  
 };
 })( jQuery );
+origParseInt = parseInt;
+parseInt = function(str) {
+		str = str.replace(/[\u06F0\u06F1\u06F2\u06F3\u06F4\u06F5\u06F6\u06F7\u06F8\u06F9]/g, function(v){return String.fromCharCode(v.charCodeAt(0) - 1728)}).replace(/[\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669]/g, function(v){return String.fromCharCode(v.charCodeAt(0) - 1584)}).replace(/[\u066B]/g, '.');
+	return origParseInt(str);	
+	};
+origParseFloat = parseFloat;
+parseFloat = function(str) {
+		str = str.replace(/[\u06F0\u06F1\u06F2\u06F3\u06F4\u06F5\u06F6\u06F7\u06F8\u06F9]/g, function(v){return String.fromCharCode(v.charCodeAt(0) - 1728)}).replace(/[\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669]/g, function(v){return String.fromCharCode(v.charCodeAt(0) - 1584)}).replace(/[\u066B]/g, '.');
+	return origParseFloat(str);	
+	};	
